1b8bd847cd3ad6e1b102ea521d4c6418f6910711
Hoang Nguyen <minh.hoang1297@gmail.com>
2020-02-01 16:33:28 -0500

    update

--- starter_code/interceptor.c
+++ starter_code/interceptor.c
@@ -441,13 +441,13 @@ static int init_function(void) {
 	// // set system call table to writeable
     // spin_lock(&calltable_lock);
     // store the original exit group syscall, and custom syscall
-    orig_custom_syscall = sys_call_table[MY_CUSTOM_SYSCALL];
-    orig_exit_group = sys_call_table[__NR_exit_group];
-    // replace with our custom exit group, and my custom syscall
-	set_addr_rw((unsigned long) sys_call_table);
-	sys_call_table[__NR_exit_group] = my_exit_group;
-	sys_call_table[MY_CUSTOM_SYSCALL] = my_syscall;
-	set_addr_ro((unsigned long) sys_call_table);
+    // orig_custom_syscall = sys_call_table[MY_CUSTOM_SYSCALL];
+    // orig_exit_group = sys_call_table[__NR_exit_group];
+    // // replace with our custom exit group, and my custom syscall
+	// set_addr_rw((unsigned long) sys_call_table);
+	// sys_call_table[__NR_exit_group] = my_exit_group;
+	// sys_call_table[MY_CUSTOM_SYSCALL] = my_syscall;
+	// set_addr_ro((unsigned long) sys_call_table);
     // spin_unlock(&calltable_lock);
 	// bookkeeping intialization
     // spin_lock(&pidlist_lock);
