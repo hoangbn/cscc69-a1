6d37d5b0fdec154aef779c6258c8a750c371b916
Hoang Nguyen <minh.hoang1297@gmail.com>
2020-02-01 20:16:47 -0500

    Update

--- starter_code/interceptor.c
+++ starter_code/interceptor.c
@@ -342,11 +342,27 @@ asmlinkage long interceptor(struct pt_regs reg) {
  *   you might be holding, before you exit the function (including error cases!).  
  */
 asmlinkage long my_syscall(int cmd, int syscall, int pid) {
-    // TODO: spinlocks checks!
+    // keep all error checking at beginning
 	mytable cur_table;
+    // check cmd validity
+    if (cmd != REQUEST_SYSCALL_INTERCEPT && cmd != REQUEST_SYSCALL_RELEASE &&
+        cmd != REQUEST_START_MONITORING && cmd != REQUEST_STOP_MONITORING) return -EINVAL;
     // check syscall validity
     if (syscall < 0 || syscall > NR_syscalls || syscall == MY_CUSTOM_SYSCALL) return -EINVAL;
-	cur_table = table[syscall];
+	// check permission of all commands and pid validity for last 2 commands 
+    // if (cmd == REQUEST_SYSCALL_INTERCEPT || cmd == REQUEST_SYSCALL_RELEASE) {
+    //     if (current_uid() != 0) return -EPERM;
+    // } else {
+    //     if (pid < 0 || (pid != 0 && pid_task(find_vpid(pid), PIDTYPE_PID) == NULL)) return -EINVAL;
+    //     if (current_uid() != 0 && (pid == 0 || check_pid_from_list(current->pid, pid) != 0)) {
+    //         return -EPERM;
+    //     }
+    // }
+    // cur_table = table[syscall];
+    // check context of commands, and busy conditions will be checked within
+    // if (cur_table.intercepted == 0 && cmd != REQUEST_SYSCALL_INTERCEPT) return -EINVAL;
+    // if ((cur_table.intercepted == 1 && cmd == REQUEST_SYSCALL_INTERCEPT) return -EBUSY; 
+    
     // if cmd is 1 of the first 2
     if (cmd == REQUEST_SYSCALL_INTERCEPT || cmd == REQUEST_SYSCALL_RELEASE) {
         // check if user is root
@@ -354,23 +370,29 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
         // if intercepting
         if (cmd == REQUEST_SYSCALL_INTERCEPT) {
             // return busy if syscall was already intercepted
-            if (cur_table.intercepted != 0) return -EBUSY;
+            if (table[syscall].intercepted != 0) return -EBUSY;
+            // store original syscall, hijack syscall, updated intercepted status
+            spin_lock(&pidlist_lock);
             spin_lock(&calltable_lock);
+            table[syscall].f = sys_call_table[syscall];
             set_addr_rw((unsigned long) sys_call_table);
-            cur_table.f = sys_call_table[syscall];
-            set_addr_ro((unsigned long) sys_call_table);
             sys_call_table[syscall] = interceptor;
+            set_addr_ro((unsigned long) sys_call_table);
+            table[syscall].intercepted = 1;
+            spin_unlock(&pidlist_lock);
             spin_unlock(&calltable_lock);
-            cur_table.intercepted = 1;
         } else { // if trying to release
             // return invalid if call was never intercepted before
-            if (cur_table.intercepted != 1) return -EINVAL;
+            if (table[syscall].intercepted != 1) return -EINVAL;
+            spin_lock(&calltable_lock);
+            spin_lock(&pidlist_lock);
             set_addr_rw((unsigned long) sys_call_table);
             sys_call_table[syscall] = cur_table.f;
             set_addr_ro((unsigned long) sys_call_table);
             destroy_list(syscall);
+            table[syscall].intercepted = 0;
+            spin_unlock(&pidlist_lock);
             spin_unlock(&calltable_lock);
-            cur_table.intercepted = 0;
         }
     }
     // else { // if cmd is 1 of 2 last
@@ -442,12 +464,12 @@ static int init_function(void) {
     spin_lock_init(&calltable_lock);
     spin_lock(&calltable_lock);
     // store the original exit group syscall, and custom syscall
-    orig_custom_syscall = sys_call_table[MY_CUSTOM_SYSCALL];
     orig_exit_group = sys_call_table[__NR_exit_group];
+    orig_custom_syscall = sys_call_table[MY_CUSTOM_SYSCALL];
     // replace with our custom exit group, and my custom syscall
 	set_addr_rw((unsigned long) sys_call_table);
-	sys_call_table[MY_CUSTOM_SYSCALL] = my_syscall;
     sys_call_table[__NR_exit_group] = my_exit_group;
+	sys_call_table[MY_CUSTOM_SYSCALL] = my_syscall;
 	set_addr_ro((unsigned long) sys_call_table);
     spin_unlock(&calltable_lock);
 	// bookkeeping intialization
@@ -475,17 +497,16 @@ static int init_function(void) {
 static void exit_function(void)
 {        
 	int i;
-    // free memory used
+    // release all intercepted system calls
     spin_lock(&pidlist_lock);
     for (i = NR_syscalls; i >= 0; i--) {
         // restore original syscall if it was intercepted
         if (table[i].intercepted == 1) {
             my_syscall(REQUEST_SYSCALL_RELEASE, i, 0);
         }
-        destroy_list(i);
     }
     spin_unlock(&pidlist_lock);
-    // restore syscalls
+    // restore original system calls
     spin_lock(&calltable_lock);
 	set_addr_rw((unsigned long) sys_call_table);
 	sys_call_table[__NR_exit_group] = orig_exit_group;
